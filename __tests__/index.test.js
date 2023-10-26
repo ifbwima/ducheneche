a63c3bf18462ac93563c6574e7082fe0415caa11
Todd Chavez <84374128+ifbwima@users.noreply.github.com>
2023-10-26 10:00:46 +0200

    jest test

---
+++ __tests__/index.test.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Home from "@/pages/index";
+import '@testing-library/jest-dom'
+
+test("renders header text", () => {
+  render(<Home />);
+  const headerElement = screen.getByText(/Test de test/i);
+  expect(headerElement).toBeInTheDocument();
+});
